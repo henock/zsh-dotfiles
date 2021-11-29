9394a5269e4395f27eda7565b0cef4ea538e37eb
henock <henock@zewde.com>
2021-11-29 17:28:26 +0000

    Adding watch function (missing from OSX).

--- extensions/.functions_extension.zsh
+++ extensions/.functions_extension.zsh
@@ -5,3 +5,21 @@ if [ $? -eq 0 ]; then
         git diff --no-index --color-words "$@";
     }
 fi;
+
+
+function watch() {
+  echo "$#"
+  if [ $# -ne 2 ]; then
+    echo -e "\n\nUsage  :  watch <sleep seconds> <command>\n\n"
+    echo -e "examples   :  watch 2 ls"
+    echo -e "           :  watch 2 'ls -l | grep bob'"
+  else
+     while :; do
+      clear
+      date
+      echo ""
+      bash -c "$2"
+      sleep "${1}"
+    done
+  fi
+}
