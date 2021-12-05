a13036d88a27d22f4ceeb007f43b4932d8a24dce
henock <henock@zewde.com>
2021-12-05 07:42:30 +0000

    Adding dos-to-unit function

--- extensions/.functions_extension.zsh
+++ extensions/.functions_extension.zsh
@@ -23,3 +23,16 @@ function watch() {
     done
   fi
 }
+
+
+function dos-to-unix() {
+  if [[ $# -eq 0 ]]; then
+    echo -e "\nUsage: dos-to-unix <input-file> [output-file]\n"
+    echo -e 'if the $2 is omitted then;  $1 convert  "$1-dos-to-unix-step" && mv "$1-dos-to-unix-step" $1'
+  elif [[ $# -eq 1 ]]; then
+    DOS_TO_UNIS_STEP_FILE="$1.dos_to_unix_step"
+    cat "$1" | awk '{ sub ("\r$", "" ); print }' > "$DOS_TO_UNIS_STEP_FILE" && mv "$DOS_TO_UNIS_STEP_FILE" "$1"
+  elif [[ $# -eq 2 ]] then
+    cat "$1" | awk '{ sub ("\r$", "" ); print }' > $2
+  fi
+}
