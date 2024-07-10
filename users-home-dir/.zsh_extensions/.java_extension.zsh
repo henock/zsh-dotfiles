d4235982ce26506689d97756de777fef96ec3736
Henock Zewde <henock@zewde.com>
2024-07-10 18:51:15 +0100

    Bugfix for exporting Java21 alias

--- users-home-dir/.zsh_extensions/.java_extension.zsh
+++ users-home-dir/.zsh_extensions/.java_extension.zsh
@@ -4,13 +4,13 @@
 export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
 export JAVA_11_HOME=$(/usr/libexec/java_home -v11)
 export JAVA_17_HOME=$(/usr/libexec/java_home -v17)
-export JAVA_21_HOME=$(/usr/libexec/java_home -v9)
+export JAVA_21_HOME=$(/usr/libexec/java_home -v21)
 
 ## Java alias
 alias java8='export JAVA_VERSION=1.8; export JAVA_HOME=$JAVA_8_HOME'
 alias java11='export JAVA_VERSION=11; export JAVA_HOME=$JAVA_11_HOME'
 alias java17='export JAVA_VERSION=17; export JAVA_HOME=$JAVA_17_HOME'
-alias java21='export JAVA_VERSION=9; export JAVA_HOME=$JAVA_21_HOME'
+alias java21='export JAVA_VERSION=21; export JAVA_HOME=$JAVA_21_HOME'
 
 
 #Maven
