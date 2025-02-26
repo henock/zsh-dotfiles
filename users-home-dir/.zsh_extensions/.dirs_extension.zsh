282fc716c4a5e21fe157c1f04c4b3e79ad2adbc2
Henock Zewde <henock@zewde.com>
2025-02-26 20:53:22 +0000

    Improved dir aliases

--- users-home-dir/.zsh_extensions/.dirs_extension.zsh
+++ users-home-dir/.zsh_extensions/.dirs_extension.zsh
@@ -10,24 +10,18 @@ setopt pushdminus
 
 # alias -g ... means that its a global alias (and therefore can be used with pipe command.. eg: cat file.txt | grep bob | .../bobs.txt )
 alias -g ..='cd ..'
-alias -g ...='cd ../..'
-alias -g ....='cd ../../..'
-alias -g .....='cd ../../../..'
-alias -g ......='cd ../../../../..'
+alias -g .2='cd ../..'
+alias -g .3='cd ../../..'
+alias -g .4='cd ../../../..'
+alias -g .5='cd ../../../../..'
 
 alias -- -='cd -'
 alias 1='cd -'
 alias 2='cd -2'
 alias 3='cd -3'
-alias 4='cd -4'
-alias 5='cd -5'
-alias 6='cd -6'
-alias 7='cd -7'
-alias 8='cd -8'
-alias 9='cd -9'
 
-alias md='mkdir -p'
-alias rd=rmdir
+alias md='mkdir -vp'
+alias rd='rmdir -i'
 
 
 # clear the screen
