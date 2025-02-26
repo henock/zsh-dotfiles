5233747c7cc28427415030a9de865f1d0b21b110
Henock Zewde <henock@zewde.com>
2025-02-26 21:55:13 +0000

    Adding java specific config

--- users-home-dir/.zsh_extensions/.java_extension.zsh
+++ users-home-dir/.zsh_extensions/.java_extension.zsh
@@ -5,13 +5,17 @@ export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
 export JAVA_11_HOME=$(/usr/libexec/java_home -v11)
 export JAVA_17_HOME=$(/usr/libexec/java_home -v17)
 export JAVA_21_HOME=$(/usr/libexec/java_home -v21)
+export JAVA_23_HOME=$(/usr/libexec/java_home -v23)
 
 ## Java alias
 alias java8='export JAVA_VERSION=1.8; export JAVA_HOME=$JAVA_8_HOME'
 alias java11='export JAVA_VERSION=11; export JAVA_HOME=$JAVA_11_HOME'
 alias java17='export JAVA_VERSION=17; export JAVA_HOME=$JAVA_17_HOME'
 alias java21='export JAVA_VERSION=21; export JAVA_HOME=$JAVA_21_HOME'
+alias java23='export JAVA_VERSION=23; export JAVA_HOME=$JAVA_23_HOME'
 
+# default to java 21
+export JAVA_HOME=$JAVA_21_HOME
 
 #Maven
 alias mci='mvn clean install'
